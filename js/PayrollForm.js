a85ed1b8c0478f72f5d17c7df81dee64f425a730
praveen3073 <pop3073@gmail.com>
2020-11-20 16:26:55 +0530

    Added: Employee Payroll Object Creation On Submit

--- js/PayrollForm.js
+++ js/PayrollForm.js
@@ -12,14 +12,16 @@ window.addEventListener('DOMContentLoaded', (event) => {
         } catch (e) {
             nameError.textContent = e;
         }
-    });
-    const salary = document.querySelector('#salary')
-    const output = document.querySelector('.salary-output')
+    }); 
+
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
     output.textContent = salary.value;
     salary.addEventListener('input', function () {
         output.textContent = salary.value;
     });
-});
+}); 
+
 class EmployeePayrollData{
     // getters and setters
     get id() {return this._id;}
@@ -56,8 +58,7 @@ class EmployeePayrollData{
     get startDate() {return this._startDate;}
     set startDate(startDate){
         let newDate = new Date(startDate[2],startDate[1],startDate[0]);
-        let startDateCompare = dates.compare(newDate,new Date());
-        if(startDateCompare<=0) this._startDate = newDate;
+        if(newDate <= new Date()) this._startDate = newDate;
         else throw 'Start Date is incorrect';
     }
 
@@ -70,35 +71,35 @@ class EmployeePayrollData{
     toString(){
         return "id="+this.id+" : name="+this.name+
                 " : gender="+this.gender+" : Dept="+this.department+
-                " : salary="+this.salary+" : Start Date="+empDate
+                " : salary="+this.salary+" : Start Date="+this.startDate
                 +" : Notes="+this.notes;
     }
 }
 
 let employees=new Array();
-let employeeData = new EmployeePayrollData();
-
-function save(){
+const save = () => {
     try {
+        let employeePayroll = new EmployeePayrollData();
         employeePayroll.name = document.getElementById('name').value;
         employeePayroll.profilePic = getRadioValue(document.getElementsByName('profile'));
         employeePayroll.gender = getRadioValue(document.getElementsByName('gender'));
         employeePayroll.department = getCheckBoxValue(document.getElementsByClassName('checkbox'));
-        employeePayroll.salary = output.textContent;
+        employeePayroll.salary = document.getElementById('salary').value;
 
-        let start=new Array();
-        start.push(getElementById('day').value);
-        start.push(getElementById('month').value);
-        start.push(getElementById('year').value);
+        let start = new Array();
+        start.push(document.getElementById('day').value);
+        start.push(document.getElementById('month').value);
+        start.push(document.getElementById('year').value);
         employeePayroll.startDate = start;
 
-        employeePayroll.notes = document.getElementById('notes').value
+        employeePayroll.notes = document.getElementById('notes').value;
         console.log(employeePayroll);
+        alert(employeePayroll);
     }
     catch (exception) {
-        console.error(exception)
+        console.error(exception); alert(exception);
     }
-    employees.push(employeePayroll)
+    employees.push(employeePayroll);
 }
 
 function getRadioValue(radios) {
