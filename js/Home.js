7b1fdc5ea7a0922403aafa2ddbae4c2eb197812d
praveen3073 <pop3073@gmail.com>
2020-11-20 19:19:41 +0530

    Added: View Multiple Employee Payroll Details Using Template Literals And For Loop

--- js/Home.js
+++ js/Home.js
@@ -11,22 +11,58 @@ function createInnerHTML(){
         "<th>Salary</th>"+
         "<th>Start Date</th>"+
         "<th>Actions</th>";
-
-    const innerHTML=`${headerHTML}
-    <tr>
-        <td><img class="profile" src="../assets/profile-images/Ellipse -5.png"></td>
-        <td>Narayan Mahadevan</td>
-        <td>Male</td>
-        <td><div class="dept-label">HR</div>
-            <div class="dept-label">FINANCE</div>
-        </td>
-        <td>RS 300000</td>
-        <td>1 Nov 2020</td>
-        <td>
-            <img id="1" onclick="remove()" alt="delete" src="../assets/icons/delete-black-18dp.svg">
-            <img id="1" onclick="update()" alt="edit" src="../assets/icons/create-black-18dp.svg">
-        </td>
-    </tr>
-    `;
+    
+    let innerHTML = `${headerHTML}`;    
+    let empPayrollList = createEmployeePayrollJson();
+    for(const empData of empPayrollList) {
+        innerHTML = `${innerHTML}
+        <tr>
+            <td><img class="profile" src="${empData._profilePic}" alt="Profile Pic"></td>
+            <td>${empData._name}</td>
+            <td>${empData._gender}</td>
+            <td>${getDeptHTML(empData._department)}</td>
+            <td>RS ${empData._salary}</td>
+            <td>${empData._startDate}</td>
+            <td>
+                <img id="${empData._id}" onclick="remove()" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+                <img id="${empData._id}" onclick="update()" alt="edit" src="../assets/icons/create-black-18dp.svg">
+            </td>
+        </tr>
+        `;
+    }
     document.querySelector('#table-display').innerHTML = innerHTML;
+}
+
+function getDeptHTML(deptList) {
+    let deptHTML = '';
+    for(const dept of deptList) {
+        deptHTML = `${deptHTML}
+        <div class="dept-label">${dept}</div>`;
+    }
+    return deptHTML;
+}
+function createEmployeePayrollJson(){
+    let empPayrollLocalList = [
+        {
+            _name:'Narayan Mahadevan',
+            _gender:'Male',
+            _department:['Engineering','Finance'],
+            _salary:'500000',
+            _startDate:'29 Oct 2019',
+            _note:'',
+            _id: new Date().getTime(),
+            _profilePic:'../assets/profile-images/Ellipse -5.png'
+        },
+        {
+            _name:'Amarpa Shashanka',
+            _gender:'Female',
+            _department:['Sales'],
+            _salary:'400000',
+            _startDate:'18 Nov 2020',
+            _note:'',
+            _id: new Date().getTime()+1,
+            _profilePic:'../assets/profile-images/Ellipse -7.png'
+        }
+    ]
+    return empPayrollLocalList;
 }
